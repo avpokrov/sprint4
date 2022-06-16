f100c5c0141443c50f5e822fc45e3423fb2b6865
Tedr <avpokrov@gmail.com>
2022-06-16 08:36:53 +0300

    fix: Added two popup, refactor code js

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,79 @@
-const popup = document.querySelector('.popup');
-const editButton = document.querySelector('.profile__edit-button');
-const closePopupButton = document.querySelector('.popup__close');
+const popupEditProfile = document.querySelector('.popup-edit-profile');
+const openButtonEditProfile = document.querySelector('.profile__edit-button');
+const closeButtonProfile = popupEditProfile.querySelector('.popup__close');
+
+const popupAddCard = document.querySelector('.popup-cardadd');
+const openButtonAddCard = document.querySelector('.profile__add-button');
+const closeButtonAddCard = popupAddCard.querySelector('.popup__close button');
+
+const openPopup = popupElement => {
+  popupElement.classList.add('popup__open');    
+}
+
+const closePopup = popupElement => {
+  popupElement.classList.remove('popup__open');    
+}
+
+
 const formPopup = document.querySelector('.popup__form');
 const nameProfile = document.querySelector('.profile__title');
 const infoProfile = document.querySelector('.profile__subtitle');
-const toFormNameProfile = popup.querySelector('.popup__input_name');
-const toFormInfoProfile = popup.querySelector('.popup__input_info');
+const toFormNameProfile = popupEditProfile.querySelector('.popup__input_name');
+const toFormInfoProfile = popupEditProfile.querySelector('.popup__input_info');
 
+const cardTemlate = document.querySelector('#card').content;
+const cards = document.querySelector('.cards');
 
-editButton.addEventListener('click', function() {
-    
-    toFormNameProfile.value = nameProfile.textContent;
-    toFormInfoProfile.value = infoProfile.textContent;
-    openPopup(popup);
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+const addCard = (name,url) => {
+    const newCard = cardTemlate.querySelector('.card').cloneNode(true);
+    newCard.querySelector('.card__img').src = url;
+    newCard.querySelector('.card__title').textContent = name;
+    cards.insertAdjacentElement('afterbegin', newCard);
+
+}
+
+initialCards.forEach((item) =>{
+   addCard(item.name, item.link);
+
 });
 
-closePopupButton.addEventListener('click', function(event){
+openButtonEditProfile.addEventListener('click', () => {
+    
+    // toFormNameProfile.value = nameProfile.textContent;
+    // toFormInfoProfile.value = infoProfile.textContent;
+    openPopup(popupEditProfile);
+});
+
+closeButtonProfile.addEventListener('click', function(event){
     event.stopPropagation;
-    closePopup(popup);
+    closePopup(popupEditProfile);
     
 });
 
@@ -34,10 +90,5 @@ formPopup.addEventListener('submit', function(event){
     closePopup(popup);
 });
 
-function openPopup(popupElement) {
-    popupElement.classList.add('popup__open');    
-}
 
-function closePopup(popupElement) {
-    popupElement.classList.remove('popup__open');    
-}
+

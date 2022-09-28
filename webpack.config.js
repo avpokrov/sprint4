29f77da6e2103631c7b875c325c0e30f66836b7b
Tedr <avpokrov@gmail.com>
2022-09-28 08:21:45 +0300

    ref: add webpack css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+
 
 module.exports = {
     entry: { main: './src/pages/index.js' },
@@ -13,5 +17,27 @@ module.exports = {
         compress: true,
         port: 8080,  
         open: true 
-    }
+    },
+    module: {
+        rules: [ {test: /\.js$/,
+            use: 'babel-loader',
+            exclude: '/node_modules/'
+          }, 
+          {test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+           type: 'asset/resource'
+          },
+          {test: /\.css$/,           
+           use: [MiniCssExtractPlugin.loader, {
+           loader: 'css-loader'
+            }]
+          }]
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+          }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
+      ]  
+    
 }
